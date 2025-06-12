b689f6c62c75a22239af515f739328ff92ad7630
Himanshu <shimanshu8181@gmail.com>
2025-06-12 19:21:34 +0530

    Update 2_problem.py

--- python/2_problem.py
+++ python/2_problem.py
@@ -3,4 +3,4 @@ num = [1,2,3,4,5,6,7,8]
 for index,item in enumerate(num):
     if index == 2 or index == 4 or index == 6:
         print(f"Value at index {index} is {item}")
-print("End of the program\nthankyou")
+print"End of the program\nthankyou")
